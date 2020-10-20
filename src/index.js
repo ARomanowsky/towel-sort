1b317dc656853aed9af0f9b5aa061a5684d6af71
Aliaksandr <51595644+ARomanowsky@users.noreply.github.com>
2020-10-20 21:44:27 +0300

    refactor: formatting

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+   if (matrix === undefined || matrix.length === 0) {return [];}
+  		// create an empty array for adding result
+      let array = [];
+    for (let i = 0; i < matrix.length; i++) {
+     if (i % 2 === 0) { 
+        array = array.concat(matrix[i]);   
+      } else { array = array.concat( matrix[i].reverse() ); 
+       }
+    }
+    return array;
+ }
